957dc2c945b37f77426aae3195653cc3e4595268
Kris Rogos <krisrogos@gmail.com>
2017-02-24 16:12:59 +0000

    [0.1.17.055.A] - Project Create
    Unreal project generated

---
+++ Source/DroidDefense/DroidDefenseGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "DroidDefenseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DROIDDEFENSE_API ADroidDefenseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
