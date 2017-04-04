4253278080e2bf7bb9e19d3a6166908bea51a076
Kris Rogos <krisrogos@gmail.com>
2017-04-04 17:10:15 +0100

    [0.1.17.094.A] - Added more droid types
    + Tank added
    + Havey droid added

--- Source/DroidDefense/C_EnemySpawn.cpp
+++ Source/DroidDefense/C_EnemySpawn.cpp
@@ -7,128 +7,147 @@
 // Sets default values
 AC_EnemySpawn::AC_EnemySpawn()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+    // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
 
     static ConstructorHelpers::FObjectFinder<UBlueprint> bp_Droid (TEXT ("Blueprint'/Game/DroidDefense/Units/BP_DroidUnit.BP_DroidUnit'"));
     if (bp_Droid.Object) mBP_DroidUnit = (UClass*)bp_Droid.Object->GeneratedClass;
-	
-	FUnitSpawnOrder nOrder1;
-	nOrder1.type = EUnitTypes::BasicUnit;
-	nOrder1.amount = 5;
-	nOrder1.spawned = 0;
-	nOrder1.delayBetween = 1.0f;
-	nOrder1.delayAfter = 10.0f;
 
-	FWave nWave1;
-	nWave1.spawnIndicator = 0;
-	nWave1.timer = 0;
-	nWave1.spawnList.Add (nOrder1);
+    static ConstructorHelpers::FObjectFinder<UBlueprint> bp_Heavy (TEXT ("Blueprint'/Game/DroidDefense/Units/BP_HeavyUnit.BP_HeavyUnit'"));
+    if (bp_Heavy.Object) mBP_HeavyUnit = (UClass*)bp_Heavy.Object->GeneratedClass;
+    
+    FUnitSpawnOrder nOrder1;
+    nOrder1.type = EUnitTypes::BasicUnit;
+    nOrder1.amount = 5;
+    nOrder1.spawned = 0;
+    nOrder1.delayBetween = 1.0f;
+    nOrder1.delayAfter = 10.0f;
 
-	mr_Waves.Add (nWave1);
+    FWave nWave1;
+    nWave1.spawnIndicator = 0;
+    nWave1.timer = 0;
+    nWave1.spawnList.Add (nOrder1);
 
-	FUnitSpawnOrder nOrder2;
-	nOrder2.type = EUnitTypes::BasicUnit;
-	nOrder2.amount = 10;
-	nOrder2.spawned = 0;
-	nOrder2.delayBetween = 0.8f;
-	nOrder2.delayAfter = 10.0f;
+    mr_Waves.Add (nWave1);
 
-	FUnitSpawnOrder nOrder3;
-	nOrder3.type = EUnitTypes::BasicUnit;
-	nOrder3.amount = 5;
-	nOrder3.spawned = 0;
-	nOrder3.delayBetween = 1.5f;
-	nOrder3.delayAfter = 10.0f;
+    FUnitSpawnOrder nOrder2;
+    nOrder2.type = EUnitTypes::BasicUnit;
+    nOrder2.amount = 10;
+    nOrder2.spawned = 0;
+    nOrder2.delayBetween = 0.8f;
+    nOrder2.delayAfter = 10.0f;
+
+    FUnitSpawnOrder nOrder3;
+    nOrder3.type = EUnitTypes::HeavyUnit;
+    nOrder3.amount = 5;
+    nOrder3.spawned = 0;
+    nOrder3.delayBetween = 1.5f;
+    nOrder3.delayAfter = 10.0f;
 
 
-	FWave nWave2;
-	nWave2.spawnIndicator = 0;
-	nWave2.timer = 0;
-	nWave2.spawnList.Add (nOrder2);
-	nWave2.spawnList.Add (nOrder3);
+    FWave nWave2;
+    nWave2.spawnIndicator = 0;
+    nWave2.timer = 0;
+    nWave2.spawnList.Add (nOrder2);
+    nWave2.spawnList.Add (nOrder3);
 
-	mr_Waves.Add (nWave2);
+    mr_Waves.Add (nWave2);
 
 }
 
 FText AC_EnemySpawn::GetWaveIndicator ()
 {
-	FString ret;
-	switch (m_State)
-	{
-	case EGameState::Playing:
-		ret = FString::Printf (TEXT ("%i of %i - order: %i of %i - unit: %i of %i"), m_CurrentWave + 1, mr_Waves.Num (),
-			mr_Waves[m_CurrentWave].spawnIndicator + 1, mr_Waves[m_CurrentWave].spawnList.Num (),
-			mr_Waves[m_CurrentWave].spawnList[mr_Waves[m_CurrentWave].spawnIndicator].spawned + 1, mr_Waves[m_CurrentWave].spawnList[mr_Waves[m_CurrentWave].spawnIndicator].amount);
-		break;
-	case EGameState::Deployed:
-		ret = FString::Printf (TEXT ("Final!!!"));
-		break;
-	case EGameState::Won:
-		ret = "Victory!!";
-		break;
-	case EGameState::Lost:
-		ret = "You lost!";
-		break;
-	default:
-		break;
-	}
+    FString ret;
+    switch (m_State)
+    {
+    case EGameState::Playing:
+        ret = FString::Printf (TEXT ("%i of %i - order: %i of %i - unit: %i of %i"), m_CurrentWave + 1, mr_Waves.Num (),
+            mr_Waves[m_CurrentWave].spawnIndicator + 1, mr_Waves[m_CurrentWave].spawnList.Num (),
+            mr_Waves[m_CurrentWave].spawnList[mr_Waves[m_CurrentWave].spawnIndicator].spawned + 1, mr_Waves[m_CurrentWave].spawnList[mr_Waves[m_CurrentWave].spawnIndicator].amount);
+        break;
+    case EGameState::Deployed:
+        ret = FString::Printf (TEXT ("Final!!!"));
+        break;
+    case EGameState::Won:
+        ret = "Victory!!";
+        break;
+    case EGameState::Lost:
+        ret = "You lost!";
+        break;
+    default:
+        break;
+    }
 
 
-	return FText::FromString(ret);
+    return FText::FromString(ret);
 }
 
 // Called when the game starts or when spawned
 void AC_EnemySpawn::BeginPlay()
 {
-	Super::BeginPlay();
-	
-	m_State = EGameState::Playing;
+    Super::BeginPlay();
+    
+    m_State = EGameState::Playing;
 
 }
 
 // Called every frame
 void AC_EnemySpawn::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
-	
-	if (m_CurrentWave < mr_Waves.Num())
-	{
-		mr_Waves[m_CurrentWave].timer += DeltaTime;
-		mr_Waves[m_CurrentWave].spawnList[mr_Waves[m_CurrentWave].spawnIndicator].timer += DeltaTime;
+    Super::Tick(DeltaTime);
+    
+    if (m_CurrentWave < mr_Waves.Num())
+    {
+        mr_Waves[m_CurrentWave].timer += DeltaTime;
+        mr_Waves[m_CurrentWave].spawnList[mr_Waves[m_CurrentWave].spawnIndicator].timer += DeltaTime;
 
-		if (mr_Waves[m_CurrentWave].spawnList[mr_Waves[m_CurrentWave].spawnIndicator].timer > mr_Waves[m_CurrentWave].spawnList[mr_Waves[m_CurrentWave].spawnIndicator].delayBetween)
-		{
-			mr_Waves[m_CurrentWave].spawnList[mr_Waves[m_CurrentWave].spawnIndicator].timer = 0;
+        if (mr_Waves[m_CurrentWave].spawnList[mr_Waves[m_CurrentWave].spawnIndicator].timer > mr_Waves[m_CurrentWave].spawnList[mr_Waves[m_CurrentWave].spawnIndicator].delayBetween)
+        {
+            mr_Waves[m_CurrentWave].spawnList[mr_Waves[m_CurrentWave].spawnIndicator].timer = 0;
 
-			UWorld* world = GetWorld ();
+            UWorld* world = GetWorld ();
 
-			FVector loc = GetActorLocation ();
-			FRotator rot = FRotator::ZeroRotator;
+            FVector loc = GetActorLocation ();
+            FRotator rot = FRotator::ZeroRotator;
+            
+            // chose the unit to spawn
+            switch (mr_Waves[m_CurrentWave].spawnList[mr_Waves[m_CurrentWave].spawnIndicator].type)
+            {
+            case EUnitTypes::BasicUnit:
+                mpr_Units.emplace_back (world->SpawnActor<AC_BasicUnit> (mBP_DroidUnit, loc, rot));
+                break;
+            case EUnitTypes::HeavyUnit:
+                mpr_Units.emplace_back (world->SpawnActor<AC_BasicUnit> (mBP_HeavyUnit, loc, rot));
+                break;
+            case EUnitTypes::BasicTank:
 
-			mpr_Units.emplace_back (world->SpawnActor<AC_BasicUnit> (mBP_DroidUnit, loc, rot));
-			mpr_Units.back ()->Init (FVector (3200.0f, 7200.0f, 0.0f));
+                break;
+            default:
+                break;
+            }
 
-			mr_Waves[m_CurrentWave].spawnList[mr_Waves[m_CurrentWave].spawnIndicator].spawned++;
+            
+            mpr_Units.back ()->Init (FVector (3200.0f, 7200.0f, 0.0f));
 
-			if (mr_Waves[m_CurrentWave].spawnList[mr_Waves[m_CurrentWave].spawnIndicator].spawned >= mr_Waves[m_CurrentWave].spawnList[mr_Waves[m_CurrentWave].spawnIndicator].amount)
-			{
-				mr_Waves[m_CurrentWave].spawnIndicator++;
+            mr_Waves[m_CurrentWave].spawnList[mr_Waves[m_CurrentWave].spawnIndicator].spawned++;
 
-				if (mr_Waves[m_CurrentWave].spawnIndicator >= mr_Waves[m_CurrentWave].spawnList.Num ())
-				{
-					m_CurrentWave++;
-				}
-			}
-		}
-	}
+            if (mr_Waves[m_CurrentWave].spawnList[mr_Waves[m_CurrentWave].spawnIndicator].spawned >= mr_Waves[m_CurrentWave].spawnList[mr_Waves[m_CurrentWave].spawnIndicator].amount)
+            {
+                mr_Waves[m_CurrentWave].spawnIndicator++;
 
-	if (m_CurrentWave >= mr_Waves.Num ())
-	{
-		m_State = EGameState::Deployed;
-	}
-	
+                if (mr_Waves[m_CurrentWave].spawnIndicator >= mr_Waves[m_CurrentWave].spawnList.Num ())
+                {
+                    m_CurrentWave++;
+                }
+            }
+        }
+    }
+
+    if (m_CurrentWave >= mr_Waves.Num ())
+    {
+        m_State = EGameState::Deployed;
+    }
+    
 
 
     m_Timer += DeltaTime;
--- Source/DroidDefense/C_EnemySpawn.h
+++ Source/DroidDefense/C_EnemySpawn.h
@@ -12,49 +12,50 @@ const float k_TimerMax = 10.0f;
 UENUM (BlueprintType)
 enum class EGameState : uint8
 {
-	Playing		UMETA(DisplayName = "Playing"),
-	Deployed	UMETA(DisplayName = "All units deployed"),
-	Won			UMETA(DisplayName = "Won"),
-	Lost		UMETA(DisplayName = "Lost")
+    Playing		UMETA(DisplayName = "Playing"),
+    Deployed	UMETA(DisplayName = "All units deployed"),
+    Won			UMETA(DisplayName = "Won"),
+    Lost		UMETA(DisplayName = "Lost")
 };
 
 UENUM (BlueprintType)
 enum class EUnitTypes : uint8
 {
-	BasicUnit	UMETA(DisplayName = "Melee"),
-	BasicTank	UMETA(DisplayName = "Tank")
+    BasicUnit	UMETA(DisplayName = "Melee"),
+    HeavyUnit   UMETA(DisplayName = "Heavy"),
+    BasicTank	UMETA(DisplayName = "Tank")
 };
 
 USTRUCT(BlueprintType)
 struct FUnitSpawnOrder
 {
-	GENERATED_BODY ()
+    GENERATED_BODY ()
 
-	UPROPERTY (BlueprintReadOnly)
-	EUnitTypes type;
-	UPROPERTY (BlueprintReadOnly)
-	int amount;
-	UPROPERTY (BlueprintReadOnly)
-	int spawned;
-	UPROPERTY (BlueprintReadOnly)
-	float delayBetween;
-	UPROPERTY (BlueprintReadOnly)
-	float delayAfter;
-	UPROPERTY (BlueprintReadOnly)
-	float timer;
+    UPROPERTY (BlueprintReadOnly)
+    EUnitTypes type;
+    UPROPERTY (BlueprintReadOnly)
+    int amount;
+    UPROPERTY (BlueprintReadOnly)
+    int spawned;
+    UPROPERTY (BlueprintReadOnly)
+    float delayBetween;
+    UPROPERTY (BlueprintReadOnly)
+    float delayAfter;
+    UPROPERTY (BlueprintReadOnly)
+    float timer;
 };
 
 USTRUCT (BlueprintType)
 struct FWave
 {
-	GENERATED_BODY ()
+    GENERATED_BODY ()
 
-	UPROPERTY (BlueprintReadOnly)
-	TArray<FUnitSpawnOrder> spawnList;
-	UPROPERTY (BlueprintReadOnly)
-	int spawnIndicator;
-	UPROPERTY (BlueprintReadOnly)
-	float timer;
+    UPROPERTY (BlueprintReadOnly)
+    TArray<FUnitSpawnOrder> spawnList;
+    UPROPERTY (BlueprintReadOnly)
+    int spawnIndicator;
+    UPROPERTY (BlueprintReadOnly)
+    float timer;
 };
 
 UCLASS()
@@ -62,36 +63,37 @@ class DROIDDEFENSE_API AC_EnemySpawn : public AActor
 {
     GENERATED_BODY ()
 
-        TSubclassOf<class AC_BasicUnit> mBP_DroidUnit;
+    TSubclassOf<class AC_BasicUnit> mBP_DroidUnit;
+    TSubclassOf<class AC_BasicUnit> mBP_HeavyUnit;
 
 public:	
-	// Sets default values for this actor's properties
-	AC_EnemySpawn();
+    // Sets default values for this actor's properties
+    AC_EnemySpawn();
 
-	UPROPERTY (EditAnywhere, DisplayName = "state of the game", Category = "Game")
-		EGameState m_State;
+    UPROPERTY (EditAnywhere, DisplayName = "state of the game", Category = "Game")
+        EGameState m_State;
 
     UPROPERTY (EditAnywhere, DisplayName = "timer", Category = "Enemy spawner")
         float m_Timer;
 
     std::vector<AC_BasicUnit*> mpr_Units;
     
-	TArray<FWave> mr_Waves;
+    TArray<FWave> mr_Waves;
 
-	UPROPERTY (EditAnywhere, DisplayName = "Wave counter", Category = "Enemy spawner")
-	int m_CurrentWave;
+    UPROPERTY (EditAnywhere, DisplayName = "Wave counter", Category = "Enemy spawner")
+    int m_CurrentWave;
 
-	UFUNCTION (BlueprintCallable, DisplayName = "Wave Indicator", Category = "Enemy spawner")
-		FText GetWaveIndicator ();
+    UFUNCTION (BlueprintCallable, DisplayName = "Wave Indicator", Category = "Enemy spawner")
+        FText GetWaveIndicator ();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
 
-	
-	
+    
+    
 };
